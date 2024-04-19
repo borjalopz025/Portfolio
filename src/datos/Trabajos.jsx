e738530f03e3abdccc0510c187e72c75cbf82210
borja <borjaswim1995@gmail.com>
2024-04-19 15:06:49 +0200

    vercel.json

--- src/datos/Trabajos.jsx
+++ src/datos/Trabajos.jsx
@@ -1,14 +1,14 @@
-const video = './public/videos/videos-Sportsync.mp4'
+
 
 export const Trabajos = [
     {
         'id':'1',
         'nombre': 'Sportsync',
-        'video' : video,
+        'video' : '/videos/videos-Sportsync.mp4',
         'tecnologias': ' ANGULAR - NODEJS - TYPESCRIPT - HTML - CSS - SQL - MySQL', 
         'Categorias': 'Desarrollo Web',
         'descripcion' : 'Red social centrada en fomentar el deporte mediante la organización de eventos deportivos, con el objetivo de conectar a personas que comparten intereses deportivos pero que aún no se conocen.',
-        'imagenPrevia': '/public/imagenes/imagen-deporte.jpeg',
+        'imagenPrevia': '/imagenes/imagen-deporte.jpeg',
         'url': 'https://github.com/borjalopz025/Front-Sportsync',
         'back' : 'https://github.com/borjalopz025/Back-Sportsync'
 
@@ -16,32 +16,32 @@ export const Trabajos = [
     {
         'id':'2',
         'nombre': 'Playground',
-        'video' : './public/videos/play-musica.mp4',
+        'video' : '/videos/play-musica.mp4',
         'tecnologias': 'DJANGO - PYTHON - HTML - CSS', 
         'Categorias': 'Desarrollo Web',
         'descripcion' : 'Página web diseñada con el objetivo de enseñar el funcionamiento y características de la plataforma, en el video podemos apreciar muchas de esas funcionalidades',
-        'imagenPrevia': '/public/imagenes/foto-play.png',
+        'imagenPrevia': '/imagenes/foto-play.png',
         'back' : 'https://github.com/borjalopz025/Playground-Django'
     },
     {
         'id':'3',
         'nombre': 'La Caffettiera',
-        'video' : './public/videos/cafe-musica.mp4',
+        'video' : '/videos/cafe-musica.mp4',
         'tecnologias': ' DJANGO - PYTHON - HTML - CSS', 
         'Categorias': 'Desarrollo Web',
         'descripcion' : 'Esta aplicación de cafetería combina un blog con artículos y noticias relacionados con la cafetería y el mundo del café. Además, ofrece información actualizada sobre los servicios y ofertas cambiantes de la cafetería',
-        'imagenPrevia':  '/public/imagenes/cafe-foto.png',
+        'imagenPrevia':  '/imagenes/cafe-foto.png',
         'back' : 'https://github.com/borjalopz025/Web-Empresa-Django'
 
     },
     {
         'id':'4',
         'nombre': 'BLOG-DEPORTES',
-        'video' : '/public/videos/blog-edit.mp4',
+        'video' : '/videos/blog-edit.mp4',
         'tecnologias': 'REACT - NODEJS - JS - POSTGRESQL - HTML - CSS', 
         'Categorias': 'Desarrollo Web',
         'descripcion' : 'Esta aplicación de blog permite a los usuarios crear, editar y borrar artículos fácilmente. Con una interfaz amigable',
-        'imagenPrevia':  '/public/imagenes/vite-react.png',
+        'imagenPrevia':  '/imagenes/vite-React.png',
         'url': 'https://github.com/borjalopz025/Blog_React',
         'back' : 'https://github.com/borjalopz025/Api-Blog'
     }
